df4d21ece4228a8cc4d9d16e5cf6104873693319
psharneja <psharneja@gmail.com>
2017-05-24 14:59:09 +0530

    q4

--- solutions/q4.js
+++ solutions/q4.js
@@ -5,18 +5,24 @@ const myArray = [10,4,7,9,2,3,1,8,6,5];
 
 function sortArray(myArray){
 //Write your code here
-    let newArray = myArray;
-    for( let i = 0 ; i < newArray.length ; i++ ){
-        for( let j = newArray.length ; j > i ; j-- ){
-            if ( newArray[ j ] < newArray[ j - 1 ] ){
-                temp = newArray[ j ];
-                newArray[ j ] = newArray[ j - 1 ];
-                newArray[ j - 1 ] = temp;
-            }
-            
-        }
-    }
-return newArray;
+//    let newArray = myArray;
+//    for( let i = 0 ; i < newArray.length ; i++ ){
+//        for( let j = newArray.length ; j > i ; j-- ){
+//            if ( newArray[ j ] < newArray[ j - 1 ] ){
+//                temp = newArray[ j ];
+//                newArray[ j ] = newArray[ j - 1 ];
+//                newArray[ j - 1 ] = temp;
+//            }
+//            
+//        }
+//    }
+//return newArray;
+    
+        return myArray.sort(sortNumber);
+
 }
+function sortNumber(a, b){
+        return  a - b;
+    }
 
 module.exports = sortArray;
