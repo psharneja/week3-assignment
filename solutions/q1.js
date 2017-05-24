f24f512468f06e80459db7332f990c2b348e012c
psharneja <psharneja@gmail.com>
2017-05-24 10:17:41 +0530

    question1

--- solutions/q1.js
+++ solutions/q1.js
@@ -1,6 +1,12 @@
 //Q1. Write a program to display the current date in the following format mm/dd/yyyy?
 
-module.exports = function(out){
+function checkdata(out){
   //Write your code here
+var date=new Date();
+  var d=date.getDate();
+  var m=date.getMonth();
+  var y=date.getFullYear();
   out(m+"/"+d+"/"+y);
 }
+
+module.exports = checkdata;
