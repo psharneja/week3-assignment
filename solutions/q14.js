9b975a8ad8a49a1114bf670047c2fc4cac492266
psharneja <psharneja@gmail.com>
2017-05-25 02:41:33 +0530

    q14pattern

--- solutions/q14.js
+++ solutions/q14.js
@@ -1,3 +1,15 @@
-//Q14.  Write a program to build a (pyramid) of "*" for a given number of rows. for example rows = 6 ?
+// Q14.  Write a program to build a (pyramid) of "*" for a given number of rows. for example rows = 6 ?
 
-//Write your code here
+// Write your code here
+
+
+function pyramid(lines) {
+    let stars = "*", spaces = " ";
+    console.log(spaces.repeat(lines*2-1) + stars);
+    for (i = 1 ; i < lines; i++){
+        
+        console.log( spaces.repeat((lines*2)-i)+ stars.repeat(i*2));
+    }
+}
+
+pyramid(20);
