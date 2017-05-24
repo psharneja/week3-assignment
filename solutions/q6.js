cb111af882e93c3a3be55eb181e432a217bbd8aa
psharneja <psharneja@gmail.com>
2017-05-24 23:52:11 +0530

    q6

--- solutions/q6.js
+++ solutions/q6.js
@@ -1,7 +1,9 @@
 // 6. Write a function to find the difference of two arrays?
-
-function difference(arr1,arr2) {
+function difference(arr1, arr2) {
 // Write your code here
-
+    let first = arr2.filter(x => arr1.indexOf(x) < 0 );
+    let second = arr1.filter(x => arr2.indexOf(x) < 0);
+    return second.concat(first);
 }
-module.exports = difference;
+
+ module.exports = difference;
