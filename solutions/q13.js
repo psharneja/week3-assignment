a64116b626a9a3d4c599428a3763566b8d4f8903
psharneja <psharneja@gmail.com>
2017-05-25 02:30:21 +0530

    13

--- solutions/q13.js
+++ solutions/q13.js
@@ -2,5 +2,14 @@
 
 fibonacci = function(numMax, done){
 //Write your code here
+    let arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
+    let i = 2;
+    for( i = 2; i < numMax+2 ; i++ ){
+        arr[i] = arr[i-1] + arr[i-2];
+        
+    }
+    done(arr)
 }
 module.exports = fibonacci;
