ecfce160723784030b00e6417a1caf1e85352f71
psharneja <psharneja@gmail.com>
2017-05-25 00:40:25 +0530

    q8

--- solutions/q8.js
+++ solutions/q8.js
@@ -1,10 +1,19 @@
-//Q8. Write a program which accepts email id as an input, which verified the following rules:-
+// Q8. Write a program which accepts email id as an input, which verified the following rules:-
 // 1) Should contain Uppercase (A-Z) and lowercase (a-z) English letters.
 // 2) Should contain Digits (0-9).
 // 3) Should not contain special haracters ! # $ % & ' * + - / = ? ^ ` { | } ~
 // 4) Should contain character period or dot provided that it is not the first or last character and it will not come one after the other.
 
 function valid_email(str) {
-    //Write your code here
+    // Write your code here
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (str.match(regex)) {
+        // console.log("sahi!");
+        return true;
+    } else {
+        //console.log("galt ea");
+        return false;
+    }
 }
-module.exports = valid_email;
+ module.exports = valid_email;
+// valid_email("abc@psharneja.com");
