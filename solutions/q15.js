a20cea14238f71799cc401c2c99f80fd9b0b1c6c
psharneja <psharneja@gmail.com>
2017-05-25 10:55:17 +0530

    q16

--- solutions/q15.js
+++ solutions/q15.js
@@ -14,6 +14,64 @@ var myObj =[{'name':'Arun', 'age': 30, 'occupation': "Developer"},
 {'name':'Venkat', 'age': 28, 'occupation': "Programmer"}];
 
 //Write your code here
+let arrayOccup=[];
+let occupationArr = {};
+// comparison function
+let compare = function(a, b){
+    return parseInt(b.age) - parseInt(a.age);
+}
+// duplicate removal
+function removeDuplicates(arr){
+  //Write your code here
+    var unique = arr.filter(function(elem, index, self) {
+    return index == self.indexOf(elem);
+})
+    return unique;
+}
+
+
+
+// part a
+console.log("PART A");
+myObj.forEach(function(data, i){
+    if(data.occupation === 'Programmer'){
+        console.log(data);
+    }
+    arrayOccup[i]=data.occupation;
+})
+
+// part b
+console.log("PART B");
+myObj.sort(compare);
+console.log(myObj);
+
+// part c
+console.log("PART C");
+arrayOccup = removeDuplicates(arrayOccup);
+console.log(arrayOccup);
+//
+//myObj.forEach( function (data, i){
+//    if(data.occupation in occupationArr){
+//        namer = data.occupation;
+//        occupationArr[namer].name = data.name ;
+//        occupationArr[namer].age = data.age;
+//    } else {
+//        occupationArr[data.occupation] = {
+//            name: data.name,
+//            age: data.age
+//        };
+//    }
+//})
+//console.log(occupationArr);
+let y = {};
+myObj.forEach(obj => {
+  Object.keys(obj).forEach(key => {
+    y[key] = (y[key] || []).concat([obj[key]]);
+  });
+});
+console.log(y);
+
+
 /* Uncomment the below statement - when code is written
 module.exports = {
   c: occupationArr,
--- solutions/q16.js
+++ solutions/q16.js
@@ -5,7 +5,20 @@ const temp = [];
 
 function flatten(arrArg) {
   //Write your code here
+//  let len = arrArg.length;
+//    for( i =0; i<len; i++){
+//        temp[i] = arrArg[i];
+//        temp.forEach(function(d, i){
+//            console.log(d);
+//        })
+//        console.log(temp);
+//    }
+//}
+
+return arrArg.reduce((a,b)=>a.concat(Array.isArray(b)? flatten(b):b),[]);
 }
+
+
 const newArr = flatten(arr);
 
 module.exports = newArr;
